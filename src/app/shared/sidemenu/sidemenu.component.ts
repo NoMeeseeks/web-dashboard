49fb6acce0ded5e5360d759f797d9b355bed330e
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-30 04:51:24 -0500

    creacion y configuracion del dashboard

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
+  selector: 'shared-side-menu',
   standalone: true,
   imports: [],
   templateUrl: './sidemenu.component.html',
@@ -8,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class SidemenuComponent {
 
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter(route => route && route.path)
+    .filter(route => !route.path?.includes(':'))
+
+  constructor() {
+    // const dashboardMenu = routes
+    //   .map(route => route.children ?? [])
+    //   .flat()
+    //   .filter(route => route && route.path)
+    //   .filter(route => !route.path?.includes(':'))
+  }
 }
