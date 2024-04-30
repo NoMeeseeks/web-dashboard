7ee03c61f5ca8f39a190af4a2ac90e974975eca6
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-30 05:05:49 -0500

    funcionalidad del menu

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'shared-side-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styles: ``
 })
