79dd391030cb65d8da7e637d297193cb38b7fd89
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-30 06:03:29 -0500

    relacion de inputs en standalones

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, booleanAttribute, input } from '@angular/core';
 
 @Component({
+  selector: 'app-title',
   standalone: true,
-  imports: [],
-  templateUrl: './title.component.html',
-  styles: ``
+  imports: [CommonModule],
+  template: `
+  <h1 class="text-3xl mb-5">{{title}} - {{withShadow}}</h1>
+  `
+
 })
 export class TitleComponent {
 
+  //es una nueva forma
+  @Input({ required: true }) title!: string;
+
+  //propiedad nueva
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
 }
