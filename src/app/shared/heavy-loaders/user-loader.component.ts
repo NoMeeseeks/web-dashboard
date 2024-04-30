95e39f12a2b7f4516a284332f28829b0a3c0941b
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-29 19:26:59 -0500

    creacion de archivos y directorios

---
+++ src/app/shared/heavy-loaders/user-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-user-loader',
+    standalone: true,
+    imports: [],
+    template: `
+    <h1>Hola MUNDOOOO</h1>
+    `
+})
+
+export class UserLoaderComponent { }
