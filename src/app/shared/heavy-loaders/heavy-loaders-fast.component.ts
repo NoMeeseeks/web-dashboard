8aed513a00c76e4dd5e0c63d6c39f805b3bf1884
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-02 14:08:07 -0500

    defer triggers y defer blocks

--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
   imports: [CommonModule],
   template: `
-<h1>Hola mundo</h1>
+<section [ngClass]="['w-full h-[600px]',cssClass]">
+  Heavy Loader fast
+</section>
 `
 })
 export class HeavyLoadersFastComponent {
+
+  @Input({ required: true }) cssClass!: string;
 
   constructor() {
     console.log('Hola');
