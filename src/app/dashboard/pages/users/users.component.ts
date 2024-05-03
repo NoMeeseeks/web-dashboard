c16bd5eb339a6c7ad9ae28180e8440ecd6793302
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-03 08:15:54 -0500

    peticiones http y servicios con señales

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { UsersService } from '@services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
+
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, RouterModule, CommonModule],
   templateUrl: './users.component.html',
   styles: ``
 })
 export default class UsersComponent {
 
+  public userServices = inject(UsersService)
 }
